c42c0f3471c9d0f817042c55cc8fcdf7d1beeb64
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-06 16:22:02 +0300

    Add Form component and basic styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ export default function App() {
   return (
     <div className="app">
       <Logo />
+      <Form />
     </div>
   );
 }
 
 function Logo() {
   return <h1>🏝️ Far Away 🎒</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🥰 trip?</h3>
+    </div>
+  );
 }
